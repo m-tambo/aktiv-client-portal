3e3745fe582ba7ade222e0878addc48e81cd96f3
tambo <michael.tambornino@gmail.com>
2024-10-31 12:14:48 -0600

    Add views and routing

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.q_text for q in latest_question_list])
+    return HttpResponse(output)
 
-def home_page(request):
+
+def home(request):
     return render(request, 'base.html')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'Currently viewing Question #{question_id}')
+
+
+def results(request, question_id):
+    response = f'Currently viewing the results of Question #{question_id}'
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Vote on Question #{question_id}')
+
